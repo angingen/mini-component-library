d4315415cc207dc1e891c5c1dde125cc456ce717
Jingwen Wang <angingen@Jingwens-MacBook-Pro.local>
2021-11-21 10:34:24 +1100

    Matchs solution approach

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { getDisplayedValue } from './Select.helpers';
-import * as S from './Select.styles';
+import VisuallyHidden from "../VisuallyHidden";
+import { getDisplayedValue } from "./Select.helpers";
+import * as S from "./Select.styles";
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ id = "sampleId", label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
-  const width = `${
-    displayedValue.length * 10 + S.PADDING.left + S.PADDING.right
-  }px`;
 
   return (
-    <S.SelectWrapper>
-      <S.Select
-        style={{
-          '--width': width,
-        }}
-        value={value}
-        onChange={onChange}
-      >
-        {children}
-      </S.Select>
-      <S.Icon id="chevron-down" />
-    </S.SelectWrapper>
+    <>
+      <VisuallyHidden>
+        <label htmlFor={id}>{label}</label>
+      </VisuallyHidden>
+      <S.SelectWrapper>
+        <S.Select id={id} value={value} onChange={onChange}>
+          {children}
+        </S.Select>
+        <S.Presentational>
+          {displayedValue} <S.Icon id="chevron-down" />
+        </S.Presentational>
+      </S.SelectWrapper>
+    </>
   );
 };
 
--- src/components/Select/Select.styles.js
+++ src/components/Select/Select.styles.js
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import IconBase from '../Icon';
+import { COLORS } from "../../constants";
+import IconBase from "../Icon";
 
 export const PADDING = {
   left: 16,
@@ -11,20 +11,17 @@ export const PADDING = {
 
 export const Select = styled.select`
   appearance: none;
-  font-size: 1rem;
-  color: inherit;
-  background-color: ${COLORS.gray50};
-  border-color: transparent;
-  padding: ${PADDING.vertical}px ${PADDING.right}px ${PADDING.vertical}px
-    ${PADDING.left}px;
-  border-radius: 8px;
-  outline-color: ${COLORS.primary};
-  width: var(--width);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
 `;
 
 export const SelectWrapper = styled.div`
   position: relative;
-  width: fit-content;
+  width: max-content;
   color: ${COLORS.gray700};
 
   &:hover {
@@ -35,8 +32,29 @@ export const SelectWrapper = styled.div`
 export const Icon = styled(IconBase)`
   position: absolute;
   right: 16px;
+  top: 0;
+  bottom: 0;
   margin: auto 0;
   width: 24px;
   height: 24px;
   color: inherit;
+  /** Preventing block the interaction of the hidden native selec element */
+  pointer-events: none;
+`;
+
+export const Presentational = styled.div`
+  font-size: 1rem;
+  color: inherit;
+  background-color: ${COLORS.gray50};
+  border-color: transparent;
+  padding: ${PADDING.vertical}px ${PADDING.right}px ${PADDING.vertical}px
+    ${PADDING.left}px;
+  border-radius: 8px;
+  outline-color: ${COLORS.primary};
+
+  ${Select}:focus + & {
+    outline: 1px dotted ${COLORS.primary};
+    outline: 5px auto -webkit-focus-ring-color;
+    outline-offset: 1px;
+  }
 `;
